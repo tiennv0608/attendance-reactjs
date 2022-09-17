ed592fcd638a2a56fe5b92b1cbfe2824d0e8f92f
tiennv0608 <tiennv0608@gmail.com>
2022-09-17 17:22:57 +0700

    bug login

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,6 +1,36 @@
 import React from "react";
+import axios from "axios";
 
 class LoginPage extends React.Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  handleOnChangeUsername = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  };
+
+  handleOnChangePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleLogin = () => {
+    let data = this.state;
+    console.log(data);
+    let headers = {
+      "Content-Type": "application/json",
+    };
+
+    axios.post("http://localhost:8081/login", data, headers).then((res) => {
+      console.log(res);
+    });
+  };
+
   render() {
     return (
       <>
@@ -9,14 +39,24 @@ class LoginPage extends React.Component {
         </div>
         <div>
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => this.handleOnChangeUsername(e)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => this.handleOnChangePassword(e)}
+          />
         </div>
         <div>
-          <button type="button">Login</button>
+          <button type="button" onClick={() => this.handleLogin()}>
+            Login
+          </button>
         </div>
       </>
     );
