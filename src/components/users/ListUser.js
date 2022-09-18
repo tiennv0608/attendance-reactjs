0cbb9f1378ac7f2006bba951a4121c0f23667baf
tiennv0608 <tiennv0608@gmail.com>
2022-09-18 21:53:17 +0700

    fix cors policy, show list user

--- src/components/users/ListUser.js
+++ src/components/users/ListUser.js
@@ -2,28 +2,37 @@ import React from "react";
 import axios from "axios";
 
 class ListUser extends React.Component {
+  state = {
+    users: [],
+  };
+
   async componentDidMount() {
     let authorization = "Bearer " + localStorage.getItem("token");
-    console.log(">>Check bearer token: ", authorization);
     await axios
       .get("http://localhost:8081/users", {
-        headers: {
-          Authorization: authorization.replace,
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
+        headers: { Authorization: authorization },
       })
       .then((res) => {
-        console.log(res);
+        this.setState({
+          users: res.data.object,
+        });
       });
   }
 
   render() {
+    const { users } = this.state;
+    console.log(users);
+
     return (
-      <>
-        <div>List user</div>
-        <div>List user</div>
-      </>
+      users &&
+      users.length &&
+      users.map((item, index) => {
+        return (
+          <div key={item.id}>
+            {index + 1} - {item.username}
+          </div>
+        );
+      })
     );
   }
 }
